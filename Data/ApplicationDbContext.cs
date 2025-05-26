6419b37c0551b7c973621a224771fd3b857ebf79
Taru Nigam <TARUNIGAM@yahoo.com>
2025-05-26 10:31:48 +0800

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GameCraft.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameCraft.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define your DbSets here, e.g.:
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
