0382b70272ef696252635a8d898135f92a9b9edf
Victor-ERTAI <735079049@qq.com>
2025-05-27 12:49:14 +0800

    Add files via upload

---
+++ Helps/PasswordHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+namespace GameCraft.Helpers
+{
+    public static class PasswordHelper
+    {
+        public static (string Hash, string Salt) HashPassword(string password)
+        {
+            byte[] saltBytes = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(saltBytes);
+            }
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 10000);
+            byte[] hash = pbkdf2.GetBytes(32);
+
+            return (Convert.ToBase64String(hash), Convert.ToBase64String(saltBytes));
+        }
+
+        public static bool VerifyPassword(string inputPassword, string storedHash, string storedSalt)
+        {
+            byte[] saltBytes = Convert.FromBase64String(storedSalt);
+            var pbkdf2 = new Rfc2898DeriveBytes(inputPassword, saltBytes, 10000);
+            byte[] inputHash = pbkdf2.GetBytes(32);
+            return Convert.ToBase64String(inputHash) == storedHash;
+        }
+    }
+}
