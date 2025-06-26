64f45e339b619947a53af0d601a4007f02917aa0
Alicia <jenniferannjoseph2001@gmail.com>
2025-06-26 20:07:04 +0800

    Add files via upload

---
+++ Helps/TempDataExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures; // For ITempDataDictionary
+using Newtonsoft.Json; // Make sure Newtonsoft.Json NuGet package is installed
+
+namespace GameCraft.Helpers 
+{
+    public static class TempDataExtensions
+    {
+        public static void Set<T>(this ITempDataDictionary tempData, string key, T value) where T : class
+        {
+            tempData[key] = JsonConvert.SerializeObject(value);
+        }
+
+        public static T? Get<T>(this ITempDataDictionary tempData, string key) where T : class
+        {
+            tempData.TryGetValue(key, out object? o);
+            return o == null ? null : JsonConvert.DeserializeObject<T>((string)o);
+        }
+    }
+}
